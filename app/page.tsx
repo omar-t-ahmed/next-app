6ff0cd189563f217ddc45d6fd2ef956f1fa70960
omar ahmed <omartahmed@gmail.com>
2024-07-26 00:36:51 -0400

    add api call

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ProductCard from "./components/ProductCard";
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>What would you like to ask?</h1>
       <Link href='/users'>All Users</Link>
       <ProductCard/>
     </main>
